dccd40fcc2c5ac87fce422c1d9bf8ee016dcc909
liuyao <3326334721o@qq.com>
2020-10-19 15:33:12 +0800

    增加 系统分享插件

--- src/xview-plugin-definitions.ts
+++ src/xview-plugin-definitions.ts
@@ -127,68 +127,181 @@ export interface StatusBarPlugin extends Plugin {
 /**
  * 键盘处理
  */
-export interface KeyboardPlugin extends Plugin {
+export interface KeyboardInfo {
     /**
-     * Show the keyboard. This method is alpha and may have issues
+     * Height of the heyboard.
+     *
+     * @since 1.0.0
      */
-    show(): Promise<void>;
-    /**
-     * Hide the keyboard.
-     */
-    hide(): Promise<void>;
-    /**
-     * Set whether the accessory bar should be visible on the keyboard. We recommend disabling
-     * the accessory bar for short forms (login, signup, etc.) to provide a cleaner UI
-     */
-    setAccessoryBarVisible(options: { isVisible: boolean }): Promise<void>;
-    /**
-     * Programmatically enable or disable the WebView scroll
-     */
-    setScroll(options: { isDisabled: boolean }): Promise<void>;
-    /**
-     * Programmatically set the keyboard style
-     */
-    setStyle(options: KeyboardStyleOptions): Promise<void>;
-    /**
-     * Programmatically set the resize mode
-     */
-    setResizeMode(options: KeyboardResizeOptions): Promise<void>;
-  
-    addListener(eventName: 'keyboardWillShow', listenerFunc: (info: KeyboardInfo) => void): PluginListenerHandle;
-    addListener(eventName: 'keyboardDidShow', listenerFunc: (info: KeyboardInfo) => void): PluginListenerHandle;
-    addListener(eventName: 'keyboardWillHide', listenerFunc: () => void): PluginListenerHandle;
-    addListener(eventName: 'keyboardDidHide', listenerFunc: () => void): PluginListenerHandle;
-  
-    /**
-     * Remove all native listeners for this plugin
-     */
-    removeAllListeners(): void;
-  }
-  
-  export interface KeyboardInfo {
     keyboardHeight: number;
   }
   
   export interface KeyboardStyleOptions {
+    /**
+     * Style of the keyboard.
+     *
+     * @since 1.0.0
+     */
     style: KeyboardStyle;
   }
   
   export enum KeyboardStyle {
+    /**
+     * Dark keyboard.
+     *
+     * @since 1.0.0
+     */
     Dark = 'DARK',
-    Light = 'LIGHT'
+  
+    /**
+     * Light keyboard.
+     *
+     * @since 1.0.0
+     */
+    Light = 'LIGHT',
   }
   
   export interface KeyboardResizeOptions {
+    /**
+     * Mode used to resize elements when the keyboard appears.
+     *
+     * @since 1.0.0
+     */
     mode: KeyboardResize;
   }
   
   export enum KeyboardResize {
+    /**
+     * Resizes the html body.
+     *
+     * @since 1.0.0
+     */
     Body = 'body',
+  
+    /**
+     * Resizes Ionic app
+     *
+     * @since 1.0.0
+     */
     Ionic = 'ionic',
+  
+    /**
+     * Resizes the WebView.
+     *
+     * @since 1.0.0
+     */
     Native = 'native',
-    None = 'none'
+  
+    /**
+     * Don't resize anything.
+     *
+     * @since 1.0.0
+     */
+    None = 'none',
   }
   
+  export interface KeyboardPlugin {
+    /**
+     * Show the keyboard. This method is alpha and may have issues.
+     *
+     * This method is only supported on Android.
+     *
+     * @since 1.0.0
+     */
+    show(): Promise<void>;
+  
+    /**
+     * Hide the keyboard.
+     *
+     * @since 1.0.0
+     */
+    hide(): Promise<void>;
+  
+    /**
+     * Set whether the accessory bar should be visible on the keyboard. We recommend disabling
+     * the accessory bar for short forms (login, signup, etc.) to provide a cleaner UI.
+     *
+     * This method is only supported on iPhone devices.
+     *
+     * @since 1.0.0
+     */
+    setAccessoryBarVisible(options: { isVisible: boolean }): Promise<void>;
+  
+    /**
+     * Programmatically enable or disable the WebView scroll.
+     *
+     * This method is only supported on iOS.
+     *
+     * @since 1.0.0
+     */
+    setScroll(options: { isDisabled: boolean }): Promise<void>;
+  
+    /**
+     * Programmatically set the keyboard style.
+     *
+     * This method is only supported on iOS.
+     *
+     * @since 1.0.0
+     */
+    setStyle(options: KeyboardStyleOptions): Promise<void>;
+  
+    /**
+     * Programmatically set the resize mode.
+     *
+     * This method is only supported on iOS.
+     *
+     * @since 1.0.0
+     */
+    setResizeMode(options: KeyboardResizeOptions): Promise<void>;
+  
+    /**
+     * Listen for when the keyboard is about to be shown.
+     *
+     * @since 1.0.0
+     */
+    addListener(
+      eventName: 'keyboardWillShow',
+      listenerFunc: (info: KeyboardInfo) => void,
+    ): PluginListenerHandle;
+  
+    /**
+     * Listen for when the keyboard is shown.
+     *
+     * @since 1.0.0
+     */
+    addListener(
+      eventName: 'keyboardDidShow',
+      listenerFunc: (info: KeyboardInfo) => void,
+    ): PluginListenerHandle;
+  
+    /**
+     * Listen for when the keyboard is about to be hidden.
+     *
+     * @since 1.0.0
+     */
+    addListener(
+      eventName: 'keyboardWillHide',
+      listenerFunc: () => void,
+    ): PluginListenerHandle;
+  
+    /**
+     * Listen for when the keyboard is hidden.
+     *
+     * @since 1.0.0
+     */
+    addListener(
+      eventName: 'keyboardDidHide',
+      listenerFunc: () => void,
+    ): PluginListenerHandle;
+  
+    /**
+     * Remove all native listeners for this plugin.
+     *
+     * @since 1.0.0
+     */
+    removeAllListeners(): void;
+}
+
 //
 
 export type OperatingSystem = 'ios' | 'android' | 'windows' | 'mac' | 'unknown';
@@ -324,6 +437,7 @@ export interface DevicePlugin {
    */
   getLanguageCode(): Promise<DeviceLanguageCodeResult>;
 }
+
 
 /**
  * 网络状态监听
