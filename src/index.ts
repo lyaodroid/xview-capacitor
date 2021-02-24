67e70c0481cc080b3798e3abab83af8903eee2ce
liuyao <3326334721@qq.com>
2021-02-24 17:02:06 +0800

    升级到 alpha10 Android29 版本

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-export * from "./xview-plugin-definitions";
-export * from "./web-plugins";
+import { registerPlugin } from "@capacitor/core";
+
+import type { AppPlugin } from "./definitions";
+
+const App = registerPlugin<AppPlugin>("App", {
+  web: () => import("./web").then((m) => new m.AppWeb()),
+});
+
+export * from "./definitions";
+export { App };
