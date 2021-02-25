372c5855f80901ff2643d916df2a54aba5163459
liuyao <3326334721@qq.com>
2021-02-25 11:55:41 +0800

    官方插件最新版本

--- src/index.ts
+++ src/index.ts
@@ -73,3 +73,70 @@ export { Network };
 /**
  * -----------------------------------------------------------------------------
  */
+import type { SharePlugin } from './share/definitions';
+
+const Share = registerPlugin<SharePlugin>('Share', {
+  web: () => import('./share/web').then(m => new m.ShareWeb()),
+});
+
+export * from './share/definitions';
+export { Share };
+
+/**
+ * -----------------------------------------------------------------------------
+ */
+import type { StoragePlugin } from './storage/definitions';
+
+const Storage = registerPlugin<StoragePlugin>('Storage', {
+  web: () => import('./storage/web').then(m => new m.StorageWeb()),
+});
+
+export * from './storage/definitions';
+export { Storage };
+
+/**
+ * -----------------------------------------------------------------------------
+ */
+import type { StatusBarPlugin } from './status-bar/definitions';
+
+const StatusBar = registerPlugin<StatusBarPlugin>('StatusBar');
+
+export * from './status-bar/definitions';
+export { StatusBar };
+
+/**
+ * -----------------------------------------------------------------------------
+ */
+import type { SplashScreenPlugin } from './splash-screen/definitions';
+
+const SplashScreen = registerPlugin<SplashScreenPlugin>('SplashScreen', {
+  web: () => import('./splash-screen/web').then(m => new m.SplashScreenWeb()),
+});
+
+export * from './splash-screen/definitions';
+export { SplashScreen };
+
+/**
+ * -----------------------------------------------------------------------------
+ */
+
+import type { FilesystemPlugin } from './filesystem/definitions';
+
+const Filesystem = registerPlugin<FilesystemPlugin>('Filesystem', {
+  web: () => import('./filesystem/web').then(m => new m.FilesystemWeb()),
+});
+
+export * from './filesystem/definitions';
+export { Filesystem };
+
+/**
+ * -----------------------------------------------------------------------------
+ */
+import type { CameraPlugin ,PermissionStatus} from './camera/definitions';
+
+const Camera = registerPlugin<CameraPlugin>('Camera', {
+  web: () => import('./camera/web').then(m => new m.CameraWeb()),
+});
+
+export * from './camera/definitions';
+export { Camera , PermissionStatus };
