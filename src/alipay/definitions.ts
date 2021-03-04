54e3576fa6fd11cc6eba6cd11cadacc602f92728
liuyao <3326334721@qq.com>
2021-03-04 10:51:13 +0800

    修改支付宝 回调 和 入参 key

--- src/alipay/definitions.ts
+++ src/alipay/definitions.ts
@@ -3,6 +3,8 @@ import type { LoginResult } from "../common";
 export interface AliPayPlugin {
   /**
    * 支付宝 登录 value = 使用 标准版授权参数
+   * catch (error) error.code == "6001" 登录失败
+   * 支付失败 状态码 查文档
    *
    * @since 1.0.0
    */
@@ -10,7 +12,7 @@ export interface AliPayPlugin {
 
   /**
    * 支付宝 支付 value = 使用 由后端生成支付参数
-   * catch (error) error.code == "1" 取消支付 
+   * catch (error) error.code == "6001" 支付失败
    * 支付失败 状态码 查文档
    *
    * @since 1.0.0
