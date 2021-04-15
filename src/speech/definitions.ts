45b7cd257c03b383945e6afdfaf504936e075f0b
liuyao <3326334721@qq.com>
2021-04-15 21:07:44 +0800

    增加科大讯飞语音合成插件

--- src/speech/definitions.ts
+++ src/speech/definitions.ts
@@ -79,7 +79,7 @@ export interface SpeechError {
 export interface RecognizeStatusChange {
     /**
      * 录音时 振幅 state = start 时有值
-     * 0 -> 12
+     * 0 -> 14
      *
      * @since 1.0.0
      */
@@ -90,7 +90,7 @@ export interface RecognizeStatusChange {
      *
      * @since 1.0.0
      */
-    state: SpeechState;
+    state: RecognizeState;
 
     /**
      * state == end 时 识别结束
@@ -101,7 +101,7 @@ export interface RecognizeStatusChange {
     data?: string;
 }
 
-export enum SpeechState {
+export enum RecognizeState {
     START = "start",
     LISTENING = "listening",
     END = "end",
@@ -111,4 +111,93 @@ export enum SpeechState {
 /**
  * 语音合成 tts
  */
-export interface SpeechTtsPlugin {}
+export interface SpeechTtsPlugin {
+    /**
+     * 开始识别 可以增加配置
+     *
+     * @since 1.0.0
+     */
+    start(options: { appId: string }): Promise<void>;
+
+    /**
+     * 暂停
+     *
+     * @since 1.0.0
+     */
+    pause(): Promise<void>;
+
+    /**
+     * 恢复
+     * @param options
+     */
+    resume(): Promise<void>;
+
+    /**
+     * 取消
+     *
+     * @since 1.0.0
+     */
+    cancel(): Promise<void>;
+
+    /**
+     * 离开页面时候
+     * 释放资源
+     *
+     * @since 1.0.0
+     */
+    destroy(): never;
+
+    /**
+     * Called when the push notification registration finished with problems.
+     *
+     * Provides an error with the registration problem.
+     *
+     * @since 1.0.0
+     */
+    addListener(
+        eventName: "speckError",
+        listenerFunc: (error: SpeechError) => void
+    ): Promise<PluginListenerHandle> & PluginListenerHandle;
+
+    /**
+     * Called when the push notification registration finishes without problems.
+     *
+     * Provides the push notification token.
+     *
+     * @since 1.0.0
+     */
+    addListener(
+        eventName: "eventSpeechSpeak",
+        listenerFunc: (recognize: RecognizeStatusChange) => void
+    ): Promise<PluginListenerHandle> & PluginListenerHandle;
+
+    /**
+     * Remove all native listeners for this plugin
+     *
+     * @since 1.0.0
+     */
+    removeAllListeners(): Promise<void>;
+}
+
+export interface SpeakStatusChange {
+    /**
+     * 百分比
+     *
+     * @since 1.0.0
+     */
+    percent: 0;
+
+    /**
+     * 语音合成状态
+     *
+     * @since 1.0.0
+     */
+    state: SpeakState;
+}
+export enum SpeakState {
+    START = "start",
+    PAUSE = "pause",
+    RESUME = "resume",
+    PROGRESS = "progress",
+    COMPLETE = "complete",
+}
