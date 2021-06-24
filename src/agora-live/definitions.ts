94761eba93ad4480637f40b96c2f08551675bd60
liuyao <3326334721@qq.com>
2021-06-24 16:06:45 +0800

    增加 RtmClient 及时聊天插件

--- src/agora-live/definitions.ts
+++ src/agora-live/definitions.ts
@@ -1,36 +1,132 @@
+import { PluginListenerHandle } from "@capacitor/core";
+
 export type RoomType =
-  | "multi"
-  | "single"
-  | "pk"
-  | "virtual_host"
-  | "ecommerce";
+    | "multi"
+    | "single"
+    | "pk"
+    | "virtual_host"
+    | "ecommerce";
 
 export interface ECommerceLivePlayerPlugin {
-  /**
-   * 测试 插件打开原生界面
-   *
-   * @since 1.0.0
-   */
-  play(options?: any): Promise<void>;
+    /**
+     * 测试 插件打开原生界面
+     *
+     * @since 1.0.0
+     */
+    play(options: LiveRoomOptions): Promise<void>;
 
-  /**
-   * 离开直播列表界面 关闭原生直播界面
-   * 
-   * @param options 
-   */
-  stop(options?: any): Promise<void>;
+    /**
+     * 离开直播列表界面 关闭原生直播界面
+     *
+     * @param options
+     */
+    stop(options?: any): Promise<void>;
+}
+export interface LiveRoomOptions {
+    /**
+     * 暂时 只能使用 ecommerce 类型
+     *
+     * @since 1.0.0
+     */
+    roomType: RoomType;
+
+    /**
+     * 直播间名字
+     */
+    roomName: string;
+
+    /**
+     * 直播间 roomId
+     */
+    roomId: string;
 }
 
 /**
+ * -----------------------------------------------------------------------------
  * 推流插件
  */
 
-
 export interface ECommerceLivePusherPlugin {
-  /**
-   * 带参数进入直播间准备
-   *
-   * @since 1.0.0
-   */
-  prepare(options?: any): Promise<void>;
+    /**
+     * 带参数进入直播间准备
+     *
+     * @since 1.0.0
+     */
+    prepare(options?: any): Promise<void>;
+}
+export interface RtmClientPlugin {
+    /**
+     * 登录结果监听
+     * 登录之前调用
+     *
+     * @since 1.0.0
+     */
+    addListener(
+        eventName: "loginListener",
+        listenerFunc: LoginListener
+    ): Promise<PluginListenerHandle> & PluginListenerHandle;
+
+    /**
+     * 带参数进入直播间准备
+     *
+     * @since 1.0.0
+     */
+    login(options: LoginLiveOptions): Promise<void>;
+
+    /**
+     * 退出登录
+     *
+     * @since 1.0.0
+     */
+    logout(options: LoginLiveOptions): Promise<void>;
+}
+export interface LoginLiveOptions {
+    /**
+     * 应用 AppId
+     *
+     * @since 1.0.0
+     */
+    appId: string;
+
+    /**
+     * 用户token
+     *
+     * @since 1.0.0
+     */
+    userToken: string;
+
+    /**
+     * Rtm 聊天 token
+     *
+     * @since 1.0.0
+     */
+    rtmToken: string;
+
+    /**
+     * 生成用户 uid
+     *
+     * @since 1.0.0
+     */
+    uid: string;
+}
+
+export type LoginListener = (result: RtmLoginLiveResult) => void;
+export interface RtmLoginLiveResult {
+    /**
+     * false 时  error 有值
+     *
+     * @since 1.0.0
+     */
+    success: boolean;
+
+    /**
+     * 错误信息 排查错误时有用
+     *
+     *
+     * @since 1.0.0
+     */
+    error?: {
+        code: number;
+        description: string;
+    };
 }
--- src/agora-live/index.ts
+++ src/agora-live/index.ts
@@ -3,6 +3,7 @@ import { registerPlugin } from "@capacitor/core";
 import type {
     ECommerceLivePlayerPlugin,
     ECommerceLivePusherPlugin,
+    RtmClientPlugin,
 } from "./definitions";
 
 export * from "./definitions";
@@ -20,5 +21,8 @@ export { ECommerceLivePlayer };
 const ECommerceLivePusher = registerPlugin<ECommerceLivePusherPlugin>(
     "ECommerceLivePusher"
 );
-
 export { ECommerceLivePusher };
+
+const RtmClient = registerPlugin<RtmClientPlugin>("RtmClient");
+
+export { RtmClient };
