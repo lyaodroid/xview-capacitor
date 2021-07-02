07976bcc6685b21ec290f1bd6db24eca467fd421
liuyao <3326334721@qq.com>
2021-07-02 09:57:03 +0800

    移除直播播放插件

--- src/agora-live/index.ts
+++ src/agora-live/index.ts
@@ -1,27 +1,16 @@
 import { registerPlugin } from "@capacitor/core";
 
 import type {
-    ECommerceLivePlayerPlugin,
-    ECommerceLivePusherPlugin,
-    LiveChatPlugin,
+    LiveChatPlugin, LivePusherPlugin,
 } from "./definitions";
 
 export * from "./definitions";
 
-const ECommerceLivePlayer = registerPlugin<ECommerceLivePlayerPlugin>(
-    "ECommerceLivePlayer",
-    {
-        web: () =>
-            import("./web").then((m) => new m.ECommerceLivePlayerWeb()),
-    }
-);
 
-export { ECommerceLivePlayer };
-
-const ECommerceLivePusher = registerPlugin<ECommerceLivePusherPlugin>(
-    "ECommerceLivePusher"
+const LivePusher = registerPlugin<LivePusherPlugin>(
+    "LivePusher"
 );
-export { ECommerceLivePusher };
+export { LivePusher };
 
 const LiveChat = registerPlugin<LiveChatPlugin>("LiveChat");
 
--- src/agora-live/web.ts
+++ src/agora-live/web.ts
@@ -1,14 +1,35 @@
-import { WebPlugin } from "@capacitor/core";
+import { WebPlugin, PluginListenerHandle } from "@capacitor/core";
 
-import type { ECommerceLivePlayerPlugin } from "./definitions";
+import {
+    LiveChatPlugin,
+    LoginLiveOptions,
+    LoginListener,
+} from "./definitions";
 
-export class ECommerceLivePlayerWeb
-    extends WebPlugin
-    implements ECommerceLivePlayerPlugin {
-    stop(_options?: any): Promise<void> {
+export class LiveChatWeb extends WebPlugin implements LiveChatPlugin {
+    constructor() {
+        super();
+    }
+
+    login(options: LoginLiveOptions): Promise<void> {
         throw new Error("Method not implemented.");
     }
-    play(_options?: any): Promise<void> {
+    logout(options: LoginLiveOptions): Promise<void> {
         throw new Error("Method not implemented.");
+    }
+
+    /**
+     * 登录结果监听
+     * 登录之前调用
+     *
+     * @since 1.0.0
+     */
+    addListener(
+        eventName: "loginListener",
+        listenerFunc: LoginListener
+    ): Promise<PluginListenerHandle> & PluginListenerHandle {
+        throw this.unimplemented("Not implemented on web.");
     }
 }
+
+1;
